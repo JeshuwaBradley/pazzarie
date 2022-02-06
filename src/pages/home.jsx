ed95df8f7aa90ea7e2eeca5541670510f5abba30
Jeshuwa Bradley <76804175+JeshuwaBradley@users.noreply.github.com>
2022-02-06 10:18:51 +0530

    redo homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Card from "../components/card";
 import Footer from "../components/footer";
 import Hero from "../components/hero";
 import Navbar from "../components/navbar";
@@ -9,7 +8,29 @@ const Home = () => {
 		<div className="">
 			<Navbar />
 			<Hero />
-			<div className="productList">
+			<div className="home-image-container">
+				<div className="row">
+					<div className="row-item"></div>
+					<div className="row-item text-row">
+						<h3>Welcome to edible happy pizzerie</h3>
+						<p>
+							Edible happy pizzerie proudly serves delicious food
+							to the greater oakland community
+						</p>
+					</div>
+				</div>
+				<div className="row">
+					<div className="row-item text-row">
+						<h3>Welcome to edible happy pizzerie</h3>
+						<p>
+							Edible happy pizzerie proudly serves delicious food
+							to the greater oakland community
+						</p>
+					</div>
+					<div className="row-item"></div>
+				</div>
+			</div>
+			{/* <div className="productList">
 				<div className="productList-title-container">
 					<h2 className="productList-title">Our Menu</h2>
 					<hr className="productList-hr" />
@@ -26,7 +47,7 @@ const Home = () => {
 						<Card />
 					</div>
 				</div>
-			</div>
+			</div> */}
 			<Footer />
 		</div>
 	);
