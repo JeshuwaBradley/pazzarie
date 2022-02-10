30d6166e290aaa6b8de5dbcb410283c318b3729f
Jeshuwa Bradley <76804175+JeshuwaBradley@users.noreply.github.com>
2022-02-10 09:00:48 +0530

    modified initial create-backend

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 mongoose.connect(
-    process.env.MONGO_URL
+    process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
 )
     .then(() => console.log('DB connection successfull!'))
     .catch((err) => {
