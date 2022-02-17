7220c52e56f013c32258f6668cc62585e6aa7bfe
Jeshuwa Bradley <76804175+JeshuwaBradley@users.noreply.github.com>
2022-02-17 09:58:01 +0530

    modified cart

--- client/src/pages/cart.jsx
+++ client/src/pages/cart.jsx
@@ -2,29 +2,87 @@ import React from "react";
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
 import { useDispatch, useSelector } from "react-redux";
-import { reset, deleteProduct } from "../redux/cartSlice";
-// import axios from "axios";
+import {
+	deleteProduct,
+	addDelivery,
+	deleteDelivery,
+	reset,
+} from "../redux/cartSlice";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const Cart = () => {
 	const cart = useSelector((state) => state.cart);
-	// const amount = cart.total;
-	// const currency = "USD";
+	const [deliver, setDeliver] = useState(false);
+	const [inputs, setInputs] = useState({});
 	const dispatch = useDispatch();
 
-	// const createOrder = async (data) => {
-	// 	try {
-	// 		const res = await axios.post("/api/orders", data);
-	// 		if (res.status === 201) {
-	// 			dispatch(reset());
-	// 		}
-	// 	} catch (err) {
-	// 		console.log(err);
-	// 	}
-	// };
+	const total = cart.total.toFixed(2);
+	const orderItems = [];
+	cart.products.forEach((product) => {
+		let itemName = product.itemTitle;
+		let size =
+			product.size == 0
+				? "small"
+				: product.size == 1
+				? "medium"
+				: product.size == 2
+				? "large"
+				: "";
+		let quantity = product.quantity;
+		let itemPrice = product.price.toFixed(2);
+		let itemTotal = (product.price * product.quantity).toFixed(2);
+		let extras = [];
+		product.extraOptions.map((item) => {
+			extras.push(item.text);
+		});
+		orderItems.push({
+			itemName: itemName,
+			size: size,
+			quantity: quantity,
+			itemPrice: itemPrice,
+			itemTotal: itemTotal,
+			extras: extras,
+		});
+	});
+
+	const deliverToMe = (e) => {
+		setDeliver(!deliver);
+		const checked = e.target.checked;
+		if (checked) {
+			dispatch(addDelivery(200));
+		} else {
+			dispatch(deleteDelivery(200));
+		}
+	};
 
 	const handleDelete = (x) => {
-		// console.log(x._id);
 		dispatch(deleteProduct(x));
+	};
+
+	const handleChange = (event) => {
+		const name = event.target.name;
+		const value = event.target.value;
+		setInputs((values) => ({ ...values, [name]: value }));
+	};
+
+	const handleOrder = (e) => {
+		e.preventDefault();
+		let x = {
+			...inputs,
+			orderItems: orderItems,
+			total: total,
+		};
+		axios
+			.post("http://localhost:5000/api/order/", { ...x })
+			.then((response) => {
+				console.log(response);
+				dispatch(reset());
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 	};
 
 	return (
@@ -33,118 +91,285 @@ const Cart = () => {
 			<div>
 				<div className="cart-container">
 					<div className="cart-title">
-						<h2>Your Cart</h2>
+						<h2>Cart</h2>
 						<div className="underline"></div>
 					</div>
 					<div className="cart-main">
 						<div className="cart-container-left">
 							<div className="cart-table">
-								<div className="cart-row">
-									<div className="cart-column">
-										<h3>Product</h3>
-									</div>
-									<div className="cart-column">
-										<h3>Name</h3>
-									</div>
-									<div className="cart-column">
-										<h3>Extras</h3>
-									</div>
-									<div className="cart-column">
-										<h3>Price</h3>
-									</div>
-									<div className="cart-column">
-										<h3>Quantity</h3>
-									</div>
-									<div className="cart-column">
-										<h3>Total</h3>
-									</div>
-									<div className="cart-column">
-										<h3>Actions</h3>
-									</div>
-								</div>
-								{cart?.products?.map((product) => (
-									<div className="cart-row">
-										<div className="cart-column">
-											<img
-												src={product.imgSrc}
-												layout="fill"
-												objectfit="cover"
-												className="cart-item-image"
-												alt=""
-											/>
+								{cart.products.length === 0 ? (
+									<div className="empty-cart">
+										Your cart is empty. Explore items
+										<div className="cart-menu-link">
+											<Link to="/menu">Explore</Link>
 										</div>
-										<div className="cart-column">
-											<span className="cart-name">
-												{product.itemTitle}
-											</span>
-										</div>
-										<div className="cart-column">
-											<div className="cart-extras">
-												{product.extras.map((extra) => (
-													<p key={extra._id}>
-														{extra.text},
-													</p>
-												))}
+									</div>
+								) : (
+									<div>
+										<div className="cart-row">
+											<div className="cart-column">
+												<h3>Product</h3>
+											</div>
+											<div className="cart-column">
+												<h3>Name</h3>
+											</div>
+											<div className="cart-column">
+												<h3>Extras</h3>
+											</div>
+											<div className="cart-column">
+												<h3>Price</h3>
+											</div>
+											<div className="cart-column">
+												<h3>Quantity</h3>
+											</div>
+											<div className="cart-column">
+												<h3>Total</h3>
+											</div>
+											<div className="cart-column">
+												<h3>Actions</h3>
 											</div>
 										</div>
-										<div className="cart-column">
-											<span className="cart-price">
-												$ {product.price.toFixed(2)}
-											</span>
-										</div>
-										<div className="cart-column">
-											<span className="cart-quantity">
-												{product.quantity}
-											</span>
-										</div>
-										<div className="cart-column">
-											<span className="cart-total">
-												$
-												{(
-													product.price *
-													product.quantity
-												).toFixed(2)}
-											</span>
-										</div>
-										<div className="cart-column">
-											<span
-												style={{
-													fontSize: "1.5em",
-													color: "#d1411e",
-													cursor: "pointer",
-												}}
-												onClick={() =>
-													handleDelete(product)
-												}
-											>
-												<i
-													className="fa fa-trash"
-													aria-hidden="true"
-												></i>
-											</span>
-										</div>
+										{cart?.products?.map((product, i) => (
+											<div className="cart-row" key={i}>
+												<div className="cart-column">
+													<img
+														src={product.imgSrc}
+														layout="fill"
+														objectfit="cover"
+														className="cart-item-image"
+														alt=""
+													/>
+												</div>
+												<div className="cart-column">
+													<span className="cart-name">
+														{product.itemTitle}
+													</span>
+												</div>
+												<div className="cart-column">
+													<div className="cart-extras">
+														{product.extras.map(
+															(extra) => (
+																<p
+																	key={
+																		extra._id
+																	}
+																>
+																	{extra.text}
+																	,
+																</p>
+															)
+														)}
+													</div>
+												</div>
+												<div className="cart-column">
+													<span className="cart-price">
+														$
+														{product.price.toFixed(
+															2
+														)}
+													</span>
+												</div>
+												<div className="cart-column">
+													<span className="cart-quantity">
+														{product.quantity}
+													</span>
+												</div>
+												<div className="cart-column">
+													<span className="cart-total">
+														$
+														{(
+															product.price *
+															product.quantity
+														).toFixed(2)}
+													</span>
+												</div>
+												<div className="cart-column">
+													<span
+														style={{
+															fontSize: "1.5em",
+															color: "#d1411e",
+															cursor: "pointer",
+														}}
+														onClick={() =>
+															handleDelete(
+																product
+															)
+														}
+													>
+														<i
+															className="fa fa-trash"
+															aria-hidden="true"
+														></i>
+													</span>
+												</div>
+											</div>
+										))}
 									</div>
-								))}
+								)}
+							</div>
+							<div className="total-container">
+								<div className="total-container-inner">
+									<div className="deliver-container">
+										<input
+											type="checkbox"
+											name="deliver"
+											id="deliver"
+											onChange={(e) => deliverToMe(e)}
+										/>
+										<label htmlFor="deliver">
+											Deliver the goods to me
+										</label>
+									</div>
+								</div>
+								<div className="total-container-inner">
+									<div className="total-container-text">
+										Subtotal: $
+									</div>
+									<div className="total-container-total">
+										{cart.total.toFixed(2)}
+									</div>
+								</div>
+								<div className="total-container-inner">
+									<div className="total-container-text">
+										Discount: $
+									</div>
+									<div className="total-container-total">
+										0.00
+									</div>
+								</div>
+								<div className="total-container-inner">
+									<div className="total-container-text">
+										Total to pay: $
+									</div>
+									<div className="total-container-total">
+										{cart.total.toFixed(2)}
+									</div>
+								</div>
 							</div>
 						</div>
 						<div className="cart-container-right">
-							<div className="cart-container-right-wrapper">
-								<h2 className="cart-total-title">CART TOTAL</h2>
-								<div className="totalText">
-									<b className="totalTextTitle">Subtotal:</b>$
-									{cart.total.toFixed(2)}
+							<div className="checkout-form">
+								<div className="form-title">
+									<h2>Customer Information</h2>
 								</div>
-								<div className="totalText">
-									<b className="totalTextTitle">Discount:</b>
-									$0.00
+								<div className="form">
+									<form action="">
+										<div className="form-item">
+											<div className="form-label">
+												<label htmlFor="customer">
+													Customer Name:
+												</label>
+											</div>
+											<div className="form-input">
+												<input
+													type="text"
+													name="customer"
+													placeholder="John Doe"
+													onChange={(e) =>
+														handleChange(e)
+													}
+												/>
+											</div>
+										</div>
+										<div className="form-item">
+											<div className="form-label">
+												<label htmlFor="email">
+													Customer Email:
+												</label>
+											</div>
+											<div className="form-input">
+												<input
+													type="email"
+													name="email"
+													placeholder="johndoe@email.com"
+													onChange={(e) =>
+														handleChange(e)
+													}
+												/>
+											</div>
+										</div>
+
+										{deliver && (
+											<>
+												<div className="form-item">
+													<div className="form-label">
+														<label htmlFor="address">
+															Address:
+														</label>
+													</div>
+													<div className="form-input">
+														<input
+															type="text"
+															name="address"
+															placeholder="250 W Bullard Ave"
+															onChange={
+																handleChange
+															}
+														/>
+													</div>
+												</div>
+												<div className="form-item">
+													<div className="form-label">
+														<label htmlFor="city">
+															City:
+														</label>
+													</div>
+													<div className="form-input">
+														<input
+															type="text"
+															name="city"
+															placeholder="Clovis"
+															onChange={(e) =>
+																handleChange(e)
+															}
+														/>
+													</div>
+												</div>
+												<div className="form-item">
+													<div className="form-label">
+														<label htmlFor="state">
+															State:
+														</label>
+													</div>
+													<div className="form-input">
+														<input
+															type="text"
+															name="state"
+															placeholder="California"
+															onChange={(e) =>
+																handleChange(e)
+															}
+														/>
+													</div>
+												</div>
+												<div className="form-item">
+													<div className="form-label">
+														<label htmlFor="zip">
+															ZIP:
+														</label>
+													</div>
+													<div className="form-input">
+														<input
+															type="text"
+															name="zip"
+															placeholder="93612"
+															onChange={(e) =>
+																handleChange(e)
+															}
+														/>
+													</div>
+												</div>
+											</>
+										)}
+										<div className="form-item">
+											<button
+												onClick={(e) => handleOrder(e)}
+											>
+												Continue Checkout
+											</button>
+										</div>
+									</form>
 								</div>
-								<div className="totalText">
-									<b className="totalTextTitle">Total:</b>$
-									{cart.total.toFixed(2)}
-								</div>
-								<button className="checkout-button">
-									CHECKOUT NOW!
-								</button>
 							</div>
 						</div>
 					</div>
--- models/Order.js
+++ models/Order.js
@@ -5,25 +5,69 @@ const OrderSchema = new mongoose.Schema(
         customer: {
             type: String,
             required: true,
-            maxlength: 60,
+            maxlength: 100,
+        },
+        email: {
+            type: String,
+            required: true,
+            maxlength: 100,
+        },
+        orderItems: {
+            type: [
+                {
+                    itemName: {
+                        type: String,
+                        required: true,
+                        maxlength: 60,
+                    },
+                    extras: {
+                        type: [
+                            {
+                                type: String,
+                                maxlength: 60,
+                            }
+                        ],
+                    },
+                    size: {
+                        type: String,
+                        required: true,
+                        maxlength: 60,
+                    },
+                    quantity: {
+                        type: Number,
+                        required: true,
+                    },
+                    itemPrice: {
+                        type: Number,
+                        required: true,
+                    },
+                    itemTotal: {
+                        type: Number,
+                        required: true,
+                    }
+                }
+            ],
+            required: true,
         },
         address: {
             type: String,
-            required: true,
             maxlength: 200,
+        },
+        city: {
+            type: String,
+            maxlength: 60,
+        },
+        state: {
+            type: String,
+            maxlength: 60,
+        },
+        zip: {
+            type: Number,
         },
         total: {
             type: Number,
             required: true,
-        },
-        status: {
-            type: Number,
-            default: 0,
-        },
-        method: {
-            type: Number,
-            required: true
-        },
+        }
     },
     { timestamps: true }
 );
